fbacd22ffd2f4e1df0400aa8e7b2546750548a98
MaruthiThatikonda <maruthithatikonda04@gmail.com>
2026-07-05 14:41:08 +0530

    Codeforce sheet1

---
+++ modules/calculator.py
@@ -0,0 +1,17 @@
+import addition 
+from subtraction import sub
+import multiplication as MUL
+from division import div as DIVISION
+import square
+import squareroot as sq
+import floordivision
+from modulodivision import modulo as md
+
+print(addition.add(x=10,y=20))
+print(sub(x=10,y=5))
+print(MUL.mul(x=10,y=5))
+print(DIVISION(10,5))
+print(square.square(x=5))
+print(sq.square_root(x=25))
+print(floordivision.floor_division(x=17,y=5))
+print(md(17,5))
